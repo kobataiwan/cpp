06ed2cbbdd2e8515d4b64471d69345021602b406
koba <koba.taiwan@gmail.com>
2019-11-27 08:20:50 +0800

    mergesort

---
+++ mergesort.cc
@@ -0,0 +1,84 @@
+#include <iostream>
+
+using namespace std;
+
+void 
+right_shift(
+    unsigned data[],
+    unsigned size 
+)
+{
+    unsigned tmp = data [size - 1];
+    cout << "size: " << size << endl;
+    for (unsigned i = 0; i < size; i ++) {
+        cout << data[i] << " ";
+    }
+    cout << endl;
+
+    for (signed j = (size - 2); j >= 0; j--) {
+        data[j + 1] = data [j];
+    }
+    data [0] = tmp;
+#if 0
+    cout << "after: " << endl;
+    for (unsigned i = 0; i < size; i ++) {
+        cout << data[i] << " ";
+    }
+    cout << endl;
+#endif
+}
+
+void 
+arrange (
+    unsigned ldata[],
+    unsigned ls,
+    unsigned rdata[],
+    unsigned rs) 
+{
+    cout << "ls: " << ls << "rs: " << rs << endl;
+    for (unsigned i = 0; i < ls + rs; i ++) {
+        cout << ldata[i] << " ";
+    }
+    cout << endl;
+   for (unsigned i = 0; i < rs; i ++) {
+       for (unsigned j = 0; j <= ls; j ++) {
+           if (ldata[j] > rdata[i]) {
+                right_shift (ldata + j, ls + i + 1);
+               break;
+           }
+       }
+   }
+}
+
+void 
+mergesort (
+    unsigned data[], 
+    unsigned size) 
+{
+    unsigned div = size / 2;
+    if (size == 1) {
+        return;
+    }
+    // 
+    // left
+    //
+    mergesort (data, div);
+    //
+    // right
+    //
+    mergesort (data + div, size - div);
+    //
+    // merge
+    //
+    arrange (data, div, data + div, size - div);
+    cout << "merge ";
+    for (unsigned idx = 0; idx < size; idx ++) {
+        cout << data[idx] << " ";
+    }
+    cout << endl;
+}
+
+int main () {
+	unsigned data[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    mergesort (data, 10);
+}
